2ac2846a093fc0ed51a90b5703ad94d8ab684dd3
YIFENG <yifeng@192-168-1-106.tpgi.com.au>
2022-06-13 12:10:42 +1000

    mask

--- network/avdn_net.py
+++ network/avdn_net.py
@@ -6,6 +6,7 @@ import torch.nn.functional as f
 class AVDNNet(nn.Module):
     def __init__(self, input_shape, args):
         super(AVDNNet, self).__init__()
+        self.args = args
         self.encoding = nn.Linear(input_shape, args.rnn_hidden_dim)  # 对所有agent的obs解码
         self.q = nn.Linear(args.rnn_hidden_dim, args.attention_dim, bias=False)
         self.k = nn.Linear(args.rnn_hidden_dim, args.attention_dim, bias=False)
@@ -16,14 +17,18 @@ class AVDNNet(nn.Module):
     def forward(self, q_values):
         b, t, n, nq = q_values.shape
         x = q_values.reshape(b*t, n, nq)
+        mask = x.mean(dim=2)<-999999
+        mask = mask.clone().detach()
+        mask = mask.permute(1,0)
+        if self.args.cuda:
+            mask = mask.cuda()
         obs_emb = f.relu(self.encoding(x))
 
-        q = self.q(obs_emb)
-        k = self.k(obs_emb)
+        q = self.q(obs_emb)  # b*t, n, 64
+        k = self.k(obs_emb)  # b*t, 64, n -> bt, n, n
         v = self.v(obs_emb)
 
-        o, w = self.attn(q,k,v)
-        o = o.sum(dim=1)  # b*t, nq
-        out = f.relu(self.fc1(o))
-        out = out.reshape(b, t, 1, nq)
+        o, w = self.attn(q,k,v,mask)
+        o = o.sum(dim=1)
+        out = o.reshape(b, t, 1, nq)
         return out
