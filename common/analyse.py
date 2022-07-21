d5d4ac128353507ab6a6d658c15f0f1c73cf8ba0
YIFENG <yifeng@192-168-1-106.tpgi.com.au>
2022-07-21 13:23:38 +1000

    scheduler

--- common/analyse.py
+++ common/analyse.py
@@ -4,38 +4,39 @@ import matplotlib.pyplot as plt
 
 def plt_win_rate_mean():
     path = []
-    alg_num = 3
-    episode_rewards = [[] for _ in range(alg_num)]
-    game_map = '6h_vs_8z'
-    """path.append('../result/vdn/' + game_map)
+    alg_num = 1
+    win_rates = [[] for _ in range(alg_num)]
+    game_map = '5m_vs_6m'
+    #path.append('../result/vdn/' + game_map)
     path.append('../result/qmix/' + game_map)
-    path.append('../result/qtran_base/' + game_map)
-    path.append('../result/qtran_alt/' + game_map)
-    path.append('../result/coma/' + game_map)
-    path.append('../result/central_v+commnet/' + game_map)
-    path.append('../result/central_v+g2anet/' + game_map)"""
-    path.append('../result/ddn/'+game_map)
-    path.append('../result/ddn+om/'+game_map)
-    path.append('../result/dtrans/' + game_map)
+    #path.append('../result/qtran_base/' + game_map)
+    #path.append('../result/qtran_alt/' + game_map)
+    #path.append('../result/coma/' + game_map)
+    #path.append('../result/central_v+commnet/' + game_map)
+    #path.append('../result/central_v+g2anet/' + game_map)
+    #path.append('../result/ddn/'+game_map)
+    #path.append('../result/ddn+om/'+game_map)
+    #path.append('../result/dtrans/' + game_map)
     num_run = 8
     for i in range(alg_num):
         for j in range(num_run):
             try:
-                r = np.load(path[i] + '/episode_rewards_{}.npy'.format(j))
-                episode_rewards[i].append(r[:60]) #2*60
+                r = np.load(path[i] + '/win_rates_{}.npy'.format(j))
+                win_rates[i].append(r[:400]) #2*60
             except:
                 continue
-        episode_rewards[i] = np.array(episode_rewards[i]).mean(axis=0)
-    episode_rewards = np.array(episode_rewards)
+        win_rates[i] = np.array(win_rates[i]).mean(axis=0)
+    win_rates = np.array(win_rates)
+    print(win_rates.shape)
 
     plt.figure()
-    plt.ylim(6, 16)
-    plt.plot(range(len(episode_rewards[0])), episode_rewards[0], c='b', label='ddn')
+    plt.ylim(0,1)
+    """plt.plot(range(len(episode_rewards[0])), episode_rewards[0], c='b', label='ddn')
     plt.plot(range(len(episode_rewards[1])), episode_rewards[1], c='r', label='ddn+om')
-    plt.plot(range(len(episode_rewards[2])), episode_rewards[2], c='g', label='dtrans')
-    """plt.plot(range(len(win_rates[0])), win_rates[0], c='b', label='vdn')
-    plt.plot(range(len(win_rates[1])), win_rates[1], c='r', label='qmix')
-    plt.plot(range(len(win_rates[2])), win_rates[2], c='g', label='qtran_base')
+    plt.plot(range(len(episode_rewards[2])), episode_rewards[2], c='g', label='dtrans')"""
+    #plt.plot(range(len(win_rates[0])), win_rates[0], c='b', label='vdn')
+    plt.plot(range(len(win_rates[0])), win_rates[0], c='r', label='qmix')
+    """plt.plot(range(len(win_rates[2])), win_rates[2], c='g', label='qtran_base')
     plt.plot(range(len(win_rates[3])), win_rates[3], c='y', label='qtran_alt')
     plt.plot(range(len(win_rates[4])), win_rates[4], c='c', label='coma')
     plt.plot(range(len(win_rates[7])), win_rates[7], c='#000000', label='maven')
